8c7bca5e23496f07e834b8ef3af21d5387641ddc
horatiohab <horatiohab@gmail.com>
2021-08-27 15:08:55 +0100

    Add form validation styling

--- contact.php
+++ contact.php
@@ -2,34 +2,62 @@
 
 include("inc/connection.php");
 
-$status = "";
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+$successMessage = "";
+$nameClass = $emailClass = $messageClass = "";
+$nameErr = $emailErr = $messageErr = "";
+$name = $email = $message = "";
+$nameSuccess = $emailSuccess = $messageSuccess = false;
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
 
-if(empty($name) && empty($email) && empty($message)) {
-    $status = "All fields are required.";
-} elseif(strlen($name) < 1) {
-    $status = "Please enter a valid name";
-} elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $status = "Please enter a valid email";
-} elseif(strlen($message) < 1) {
-    $status = "Please enter a message";
-} else {
-    try {
-        $sql = 'INSERT INTO contact (name, email, message) VALUES (:name, :email, :message)';
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute(['name' => $name, 'email' => $email, 'message' => $message]);
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = test_input($_POST['name']);
+    $email = test_input($_POST['email']);
+    $message = test_input($_POST['message']);
 
-        $status = "Contact form was successfully submitted, Thank you for your message!";
-        $name = "";
-        $email = "";
-        $message = "";
-    } catch(PDOException $e) {
-        echo "Form failed to send: " . $e->getMessage();
-    }    
+    if (strlen($name) < 1) {
+        $nameErr = "Please enter a valid name";
+        $nameClass = "invalid-input";
+        $nameSuccess = false;
+    } else {
+        $nameSuccess = true;
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErr = "Please enter a valid email";
+        $emailClass = "invalid-input";
+        $emailSuccess = false;
+    } else {
+        $emailSuccess = true;
+    }
+
+    if (empty($_POST["message"])) {
+        $messageErr = "Please enter the message you would like to send.";
+        $messageClass = "invalid-input";
+        $messageSuccess = false;
+    } else {
+        $messageSuccess = true;
+    }
+
+    if ($nameSuccess && $emailSuccess && $messageSuccess) {
+        try {
+            $sql = 'INSERT INTO contact (name, email, message) VALUES (:name, :email, :message)';
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(['name' => $name, 'email' => $email, 'message' => $message]);
+    
+            $successMessage = "Contact form was successfully submitted, Thank you for your message!";
+            $name = "";
+            $email = "";
+            $message = "";
+        } catch(PDOException $e) {
+            echo "Form failed to send: " . $e->getMessage();
+        }  
+    }
 }
 
 ?>
@@ -40,26 +68,29 @@ if(empty($name) && empty($email) && empty($message)) {
     <div class="contact-container">
         <div class="contact-card">
             <h1>Contact Us</h1>
-            <form action="contact.php" method="POST" class="contact-form">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST" class="contact-form">
                 <div >
                     <label for="name">Your Name</label>
-                    <input type="text" id="name" name="name" value="<?php if($_SERVER['REQUEST_METHOD'] == 'POST') echo $name ?>">
+                    <input type="text" id="name" name="name" value="<?php if($_SERVER['REQUEST_METHOD'] == 'POST') echo $name ?>" class="<?php echo $nameClass ?>">
                 </div>
 
                 <div >
                     <label for="name">Your Email</label>
-                    <input type="text" id="email" name="email" value="<?php if($_SERVER['REQUEST_METHOD'] == 'POST') echo $email ?>">
+                    <input type="text" id="email" name="email" value="<?php if($_SERVER['REQUEST_METHOD'] == 'POST') echo $email ?>" class="<?php echo $emailClass ?>">
                 </div>
 
                 <div >
                     <label for="name">Message</label>
-                    <textarea type="text" id="message" name="message" ><?php if($_SERVER['REQUEST_METHOD'] == 'POST') echo $message ?></textarea>
+                    <textarea type="text" id="message" name="message" class="<?php echo $messageClass ?>"><?php if($_SERVER['REQUEST_METHOD'] == 'POST') echo $message ?></textarea>
                 </div>
 
                 <input type="submit" id="message_submit" value="Submit">
 
                 <div class="form-status">
-                    <span><?php echo $status; ?></span>
+                    <span><?php echo $successMessage; ?></span>
+                    <span><?php echo $nameErr; ?></span>
+                    <span><?php echo $emailErr; ?></span>
+                    <span><?php echo $messageErr; ?></span>
                 </div>
             </form>  
         </div>
